e85777cd27881d12bde593a9e5ef82829279cafc
mdgrotheer <mdgrotheer@gmail.com>
2019-09-11 12:34:45 -0400

    initla commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Oh hey there! Looks like you're having some car troubles, eh? You might need a Hose, eh? 
         </p>
         <a
           className="App-link"
